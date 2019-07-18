7f9fa62c848547f429b298c525b1dfc6554ebbfb
Brian Hong <hong.matchb@gmail.com>
2019-07-18 21:39:25 +0000

    Additional validations for Address and Description

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3, 
+    message: " is too short (minimum is %{count} characters required)" }
+  validates :address, presence: true
+  validates :description, presence: true
 end
